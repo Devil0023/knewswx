6c72ba7315f19102830a6214fb58365ccab95ba3
Devil0023 <Devil0023@gmail.com>
2018-03-19 17:17:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# knewswx
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //add fixed sql
+use Encore\Admin\Config\Config;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+	Schema::defaultStringLength(191); //add fixed sql
+	Config::load();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
