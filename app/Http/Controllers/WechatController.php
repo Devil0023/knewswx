e5c1ec60314e1fd5a3bda541924ef99fea2a9530
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-21 17:53:30 +0800

    234234

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Prize;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class WechatController extends Controller
 {
 
     public function prizelist(){
-        $now  = date("Y-m-d H:i:s");
-        $list = Prize::where("checked", 1)->where("stime", "<=", $now)->where("etime", ">", $now)->get();
+
+        $time = time();
+        $key  = "KnewsWX-Prizelist-".date("YmdHi", $time);
+
+        $json = Redis::get($key);
+        if($json){
+            $list = json_decode($json, true);
+        }else{
+            $now  = date("Y-m-d H:i:s", $time);
+            $list = Prize::where("checked", 1)->where("stime", "<=", $now)->where("etime", ">", $now)->get()->toJson;
+            exit($list); die;
+        }
+
 
         var_dump($list);
     }
