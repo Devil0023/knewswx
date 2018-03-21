ec216b7b7a02bdd7c48a805125b49b744a77f830
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-21 14:47:42 +0800

    chk wxuser

--- app/Http/Middleware/KnewsRegister.php
+++ app/Http/Middleware/KnewsRegister.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Wxuser;
 use Closure;
 
 class KnewsRegister
@@ -16,7 +17,23 @@ class KnewsRegister
     public function handle($request, Closure $next)
     {
         $userinfo = session("wechat.oauth_user");
-        var_dump($userinfo);
+        $wxuser   = Wxuser::where("openid", "=", $userinfo->openid);
+
+        echo $userinfo;
+        echo $wxuser->id;
+        die;
+
+        if($wxuser->id){
+            var_dump(1);
+        }else{
+            $newuser = new Wxuser();
+            $newuser->openid = $userinfo->openid;
+            $newuser->nickname = $userinfo->nickname;
+            $newuser->sex = $userinfo->sex;
+            $newuser->province = $userinfo->province;
+            $newuser->city = $userinfo->city;
+
+        }
 
         return $next($request);
     }
