7fda966046a10c09ce232a592c6bbf1e4bb30eb9
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-22 09:39:33 +0800

    Prize

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,17 +17,29 @@ class WechatController extends Controller
 
         if(empty($json)){
             $now  = date("Y-m-d H:i:s", $time);
-            $json = Prize::where("checked", 1)->where("stime", "<=", $now)->where("etime", ">", $now)->get()->toJson();
+            $json = Prize::where("checked", 1)->where("stime", "<=", $now)->where("etime", ">", $now)->where("num", ">", 0)->get()->toJson();
             Redis::setex($key, 60, $json);
         }
 
         $list = json_decode($json, true);
         foreach($list as $key => $val){
-            echo "<a href=\"/wechat/prize/detail/".$val["id"]."\">".$val["prize"]."</a><br/>";
+            $val["left"] = Redis::llen("WXPrizePoolList-".$val["id"]);
+            echo "<a href=\"/wechat/prize/detail/".$val["id"]."\">".$val["prize"]."----剩余：".$val["left"]."----</a><br/>";
         }
     }
 
     public function prize($id){
-        echo $id;
+        $key  = "KnewsWX-PrizeDetail-".$id;
+        $json = @Redis::get($key);
+
+        if(empty($json)){
+            $json = Prize::find($id)->toJson();
+            Redis::setex($key, 60, $json);
+        }
+
+        $prize = json_decode($json, true);
+        $prize["left"] = Redis::llen("WXPrizePoolList-".$prize["id"]);
+
+        var_dump($prize);
     }
 }
