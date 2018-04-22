233c326835c340c1a7d905f85eec0b351e485e38
ZHANGLEI-PC\Zhanglei <devil0023@gmail.com>
2018-04-22 14:58:59 +0800

    234234234

--- app/Admin/Controllers/QuestioninfoController.php
+++ app/Admin/Controllers/QuestioninfoController.php
@@ -164,13 +164,18 @@ class QuestioninfoController extends Controller
 //                $qorder[$i] = $i;
 //            }
 
+            $qorder = $this->xrange(1, 100, 1);
+            foreach($qorder as $key => $val){
+                echo $key."---".$val;
+            }
+
             $form->display('id', 'ID');
 
             $form->text("question", "问题");
             $form->radio("type", "类型")->options([0 => "单选", 1 => "多选", 2 => "开放"])->default(0);
             $form->radio("isrequired", "必填")->options([0 => "否", 1 => "是"])->default(0);
             $form->textarea("options", "选项(换行区分，'其他'为else)");
-            $form->select("qorder", "题目顺序")->options($this->xrange(1, 100, 1))->default(1);
+            $form->select("qorder", "题目顺序")->options()->default(1);
 
             $form->hidden("qid")->value($this->qid);
 
